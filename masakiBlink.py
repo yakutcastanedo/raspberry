789b8e18d9083711a0b161f31cdb6051fa06681a
Masaki Ihara <masaki.ihara@itcollege.ee>
2016-09-29 09:30:32 +0000

    added blink code

---
+++ masakiBlink.py
@@ -0,0 +1,20 @@
+# import Raspberry Pi GPIO support into Python environment
+import RPi.GPIO as GPIO
+# import a sleep function from time module
+from time import sleep
+
+led = 10  # GPIO number where the led is connected
+
+# Tell the GPIO module to use GPIO numbering used by processor
+GPIO.setmode(GPIO.BCM)
+
+# Set GPIO no 10 to output mode
+GPIO.setup(led, GPIO.OUT)
+
+# Blink some leds
+while True:
+    GPIO.output(led, False)
+    sleep(1)  # Sleep for 1 second
+    GPIO.output(led, True)
+    sleep(1)
+    
