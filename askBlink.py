8d955a63791a77c511acc33aa6eee92693e5c8af
Masaki <masaki0320@icloud.com>
2016-10-06 13:13:45 +0300

    Add files via upload

---
+++ askBlink.py
@@ -0,0 +1,39 @@
+# import Raspberry Pi GPIO support into Python environment
+import RPi.GPIO as GPIO
+# import a sleep function from time module
+from time import sleep
+
+# GPIO number where the led is connected
+red = 10
+green = 22
+blue = 27
+# Tell the GPIO module to use GPIO numbering used by processor
+GPIO.setmode(GPIO.BCM)
+
+# Set GPIO no 10 to output mode
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
+
+# Blink some leds"
+
+colorList = {0: 'red',
+             1: 'green',
+             2: 'blue'}
+
+userChoise = int(input("Please choose a number (red = 0, green = 1, blue = 2): "))
+print(userChoise)
+
+
+def blink(a):
+    if a == 0:
+        GPIO.setup(red, GPIO.OUT)
+        return print("blinking red")
+    elif a == 1:
+        GPIO.setup(green, GPIO.OUT)
+        return print("blinking green")
+    elif a == 2:
+        GPIO.setup(blue, GPIO.OUT)
+        return print("blinking blue")
+
+blink(userChoise)
